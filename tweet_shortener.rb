9229babe4601497ec8d647b353a4e08447637144
Hugo Escandon <hescandon6@gmail.com>
2018-07-13 00:55:13 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -50,7 +50,7 @@ def selective_tweet_shortener(tweet)
 end
 
 def shortened_tweet_truncator(tweet)
-  
+
     if word_substituter(tweet).length > 140
       word_substituter(tweet)[0..139] + "..."
     else
